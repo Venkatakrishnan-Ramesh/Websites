fbb7cfe3a61ecda380852e8f51cb5e5b642e1ac2
Venkatakrishnan Ramesh <venkatkrishnan2003@gmail.com>
2022-11-20 16:12:16 +0530

    Portfolio

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,6 +7,7 @@ interface LayoutProps {
   children?: ReactNode
 }
 
+
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col h-screen justify-between font-content antialiased">
